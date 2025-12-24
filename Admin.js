3c71570e01411a73cd5d36a5b84ba26629413168
ignatiustripo <ignatiustripo@gmail.com>
2025-12-25 01:44:43 +0300

    Delete Admin.js

--- Admin.js
+++
@@ -1,4 +0,0 @@
-!approve <num>      - Approve user (Private mode)
-!remove <num>       - Remove user (Private mode)
-!broadcast <msg>    - Broadcast message
-!approved           - Show approved users
