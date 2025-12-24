5fd69a56c49e991fbf3219d49fab4a438a529ae0
ignatiustripo <ignatiustripo@gmail.com>
2025-12-25 01:24:22 +0300

    Create Admin.js

---
+++ Admin.js
@@ -0,0 +1,4 @@
+!approve <num>      - Approve user (Private mode)
+!remove <num>       - Remove user (Private mode)
+!broadcast <msg>    - Broadcast message
+!approved           - Show approved users
