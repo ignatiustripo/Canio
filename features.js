bfee5efd307798693bb43443b9d3b24ceb41624d
ignatiustripo <ignatiustripo@gmail.com>
2025-12-25 01:23:24 +0300

    Create features.js

---
+++ features.js
@@ -0,0 +1,4 @@
+!toggle autoViewStatus    # Enable/disable auto view status
+!toggle antiDelete        # Enable/disable anti-delete
+!toggle typingDetection   # Enable/disable typing detection
+# ... and so on for all features
