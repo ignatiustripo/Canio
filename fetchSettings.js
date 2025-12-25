2b44bd610499e8c6eeb975803de7dc9b6fac9c54
ignatiustripo <ignatiustripo@gmail.com>
2025-12-25 07:59:12 +0300

    Create fetchSettings.js

---
+++ fetchSettings.js
@@ -0,0 +1,26 @@
+const { getSettings } = require('../database/config');
+
+async function fetchSettings() {
+  const data = await getSettings();
+
+  return {
+    wapresence: data.wapresence,
+    autoread: data.autoread,
+    mode: data.mode,
+    prefix: data.prefix,
+    autolike: data.autolike,
+    autoview: data.autoview,
+    antilink: data.antilink,
+    antilinkall: data.antilinkall,
+    antidelete: data.antidelete,
+    antitag: data.antitag,
+    antibot: data.antibot,
+    welcome: data.welcome,
+    autobio: data.autobio,
+    badword: data.badword,
+    gptdm: data.gptdm, 
+    anticall: data.anticall
+  };
+}
+
+module.exports = fetchSettings;
